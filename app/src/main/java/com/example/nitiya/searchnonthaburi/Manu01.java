d9c08ab16d9bae0a3a0065b7db118a9d32d6dcdc
masterUNG <phrombutr@gmail.com>
2016-12-10 11:12:36 +0700

    Get Location OK

--- app/src/main/java/com/example/nitiya/searchnonthaburi/Manu01.java
+++ app/src/main/java/com/example/nitiya/searchnonthaburi/Manu01.java
@@ -1,15 +1,26 @@
 package com.example.nitiya.searchnonthaburi;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class Manu01 extends AppCompatActivity {
 
-
+    //Explicit
     ImageButton img01, img02, img03;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble, lngADouble;
 
 
     @Override
@@ -18,6 +29,13 @@ public class Manu01 extends AppCompatActivity {
         setContentView(R.layout.manu01);
 
         img01 = (ImageButton) findViewById(R.id.manu001);
+
+
+        //Setup Location
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false); // ไม่นำระยะห่างจากระดับน้ำทะเล มาคำนวน
 
         img01.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +71,98 @@ public class Manu01 extends AppCompatActivity {
         });
 
 
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        latADouble = 0;
+        lngADouble = 0;
+
+        //Find Location by Network
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            latADouble = networkLocation.getLatitude();
+            lngADouble = networkLocation.getLongitude();
+        }
+
+        //Find Location by GPS
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("10decV1", "lat ==> " + latADouble);
+        Log.d("10decV1", "lng ==> " + lngADouble);
+
+
+    }   // onResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
     }
-}
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        }
+
+        return location;
+    }
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+}   // Main Class
